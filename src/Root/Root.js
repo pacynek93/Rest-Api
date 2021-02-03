1bdc1dab5ff74a27fe9058b7927d19990a714c3c
Patryk <pacynek93@gmail.com>
2021-02-03 22:31:04 +0100

    'test'

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -3,7 +3,6 @@ import React from 'react';
 import ShowData from '../ShowData/ShowData';
 
 const Root = () => (
-
   <>
     <ShowData />
   </>
--- src/ShowData/ShowData.js
+++ src/ShowData/ShowData.js
@@ -6,48 +6,52 @@ const token = '768d6ecea52a20df98f01cdd53a38d393a2655c471f428f03edb8035c8dd2910'
 
 const config = { headers: { Authorization: `Bearer ${token}` } };
 
-const removeId = document.getElementById('input');
-
 const ShowData = () => {
   const [data, setData] = useState([]);
 
   const url = 'https://gorest.co.in/public-api/users';
 
-  const mapData = data?.map(item => (
-    <div className='blocks' key={item.id}>
-      <ol>{item.id}</ol>
-      <ol>{item.name}</ol>
-      <ol>{item.email}</ol>
-      <ol>{item.gender}</ol>
-      <ol>{item.status}</ol>
-    </div>
-  ));
-
   const getData = () => {
     axios.get(url)
       .then(res => setData(res.data.data));
-
-  };
-  const removeData = () => {
-    axios.delete(`${url}/${removeId.value}`, config)
-      .then(res => {
-        console.log(res.data);
-      });
   };
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [] );
+
+  const removeData = async (id) => {
+     await axios.delete(`${url}/${id}`, config)
+      .then(res => {
+          // console.log(res);
+          // console.log(id);
+          setData(res.data.data);
+          // console.log(data);
+        },
+      );
+  };
+
+  const mapData = () => {
+    console.log(data);
+    return data?.map(item => (
+      <div className='blocks' key={item.id}>
+        <ol>{item.id}</ol>
+        <ol>{item.name}</ol>
+        <ol>{item.email}</ol>
+        <ol>{item.gender}</ol>
+        <ol>{item.status}</ol>
+        <ol>
+          <button type='button' onClick={() => removeData(item.id)}>Kill me</button>
+        </ol>
+      </div>
+    ));
+  }
 
   return (
     <div>
       <ul>
-        {mapData}
+        {mapData()}
       </ul>
-      <>
-        <input id='input' placeholder='type what Id you want to remove' required/>
-        <button type='button' onClick={removeData}>Kill me</button>
-      </>
     </div>
   );
 };
