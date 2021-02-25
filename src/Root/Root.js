d5845ffbe9c14375a149520bb54e3f4358349453
Patryk <pacynek93@gmail.com>
2021-02-25 16:17:40 +0100

    fixing bugs with paths

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -5,7 +5,7 @@ import HomePage from '../HomePage/HomePage';
 import EditUser from '../EditUser/EditUser';
 import AddUser from '../AddUser/AddUser';
 import ShowPosts from '../ShowPosts/ShowPosts';
-import ShowUserInfo from '../ShowUserInfo/ShowUserInfo';
+import ShowData from '../ShowData/ShowData';
 import EditPost from '../EditPost/EditPost';
 import ShowComments from '../ShowComments/ShowComments';
 import EditComment from '../EditComment/EditComment';
@@ -18,14 +18,14 @@ const Root = () => (
     <div className='hello'>
       <nav className='navBar'>
         <Link className='navBarItem' to='/'>Home</Link>
-        <Link className='navBarItem' to='/user'>ShowData</Link>
+        <Link className='navBarItem' to='/user'>User List</Link>
         <Link className='navBarItem' to='/post'>Post List</Link>
         <Link className='navBarItem' to='/comments'>User Comments</Link>
 
       </nav>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/user' component={ShowUserInfo} />
+        <Route exact path='/user' component={ShowData} />
         <Route exact path='/user/add' component={AddUser} />
         <Route exact path='/user/:id' component={EditUser} />
         <Route exact path='/post' component={ShowPosts} />
